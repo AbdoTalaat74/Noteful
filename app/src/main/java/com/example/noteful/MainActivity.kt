b0ada9a5d525cb36b80981118cca941caa596c6a
ABDOO <Abdo.talaat740@gmail.com>
2025-01-13 23:18:29 +0200

    init

--- app/src/main/java/com/example/noteful/MainActivity.kt
+++ app/src/main/java/com/example/noteful/MainActivity.kt
@@ -4,19 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.noteful.ui.theme.MyApplicationTheme
-import com.example.noteful.ui.theme.dimens
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyApplicationTheme {
+            MyApplicationTheme(dynamicColor = false) {
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background)
+                ) {
+                }
             }
         }
     }
